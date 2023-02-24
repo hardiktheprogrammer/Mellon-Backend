1ccf8180244cd0bd81cbabd185fc2b7ecb09b554
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-02-24 05:36:09 +0530

    add Dependencs and deploy the contract

--- programs/Prediction-safroon/src/lib.rs
+++ programs/Prediction-safroon/src/lib.rs
@@ -14,8 +14,37 @@ pub mod prediction_contract {
     pub fn create_master(_ctx: Context<CreateMaster>) -> Result<()> {
         Ok(())
     }
+pub fn create_bet(
+    ctx: Context<CreateBet>,
+    amount:u64, // amount of the be4t 
+    price:f64, // price of the bet
+    duration:u32,// seconds
+    pyth_price_key, Pubkey
+)  -> Result<()> {
 
-    
+    let master = &mut ctx.accounts.master
+    let bet = &mut  ctx.accounts.bet
+
+    // Increase the Least id on Each bet Creation on the water
+
+    master.last_bet_id += 1;
+
+    bet.id = master.last_bet_id;
+    bet.pyth_price_key = pyth_price_key;
+    bet.amount = bet.amount;
+    bet.expiryts = get_unix_timestamp() + duration on i64; // duration of the bet 
+    bet.prediction_a = BetPrediction {
+        Player:ctx.account.player.key(),
+
+        price,
+
+    }
+
+
+}
+
+}
+
     #[derive(Accounts)] // Account stuct
     pub struct CreateMaster<'info> {
         #[account(
@@ -42,11 +71,19 @@ pub struct CreateBet<'info> {
     #[account(
 
         init,
-        payer=payer,
-        space=8+8+32+8+8+32+8+1+32+8+1,
+        payer=player,
+        space=8+8+32+8+8+32+8+1+32+8+1
     )]
 
-    pub bet:Account<'info,Bet> 
+    pub bet:Account<'info,Bet>,// Bet Account
 
-    #[account(mut)] 
+    #[account(mut, seeds=[MASTER_SEED],bump)] 
+
+    pub master:Account<'info,Master>
+
+    #[account(mut)]
+    pub player:Signer<'info>, // Player 
+
+    pub system_program:Program<'info,System>, // System program 
+
 }
