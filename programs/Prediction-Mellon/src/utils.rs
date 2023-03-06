5258abbb547d48380200304e2f9f18da505584da
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-03-06 11:07:46 +0530

    add validateclaimbet function and fix bug

--- programs/Prediction-Mellon/src/utils.rs
+++ programs/Prediction-Mellon/src/utils.rs
@@ -16,8 +16,10 @@ pub fn validate_enter_bet(bet: &Bet) -> bool {
 
 pub fn validate_claim_bet(bet: &Bet) -> bool {
     match bet.state {
-        BetStated::started => {
-            let current_ts = get_unix_timestamp();
+        BetStated::Started => {
+            let current_ts = get_unix_timestamp(); // current timestamp
+            let time_passed_since_expiry = current_ts - bet.expiry_ts; // time passed since expiation
+            0 < time_passed_since_expiry && time_passed_since_expiry <= bet.expiry_ts;
         }
     }
 }
